0955d8c3009c16f8bd7dbc7ccf754414dc56cef6
Nelson Oseguera <nelsonoseguera@outlook.com>
2025-03-22 15:48:01 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import time
+import re
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
